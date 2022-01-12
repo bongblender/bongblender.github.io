526822fd94f76d01bcb9fdd77906adb9b9d66f6a
Bongblender <41033357+bongblender@users.noreply.github.com>
2022-01-12 10:42:32 +0530

    update

--- script.js
+++ script.js
@@ -1,15 +1,32 @@
+let nav = document.getElementById("Nav")
+let tab_button = document.getElementById("tab-button")
+let side_nav = document.getElementById("tab-for-mobile")
 
-var button = document.getElementById("ham-button");
-
-
-// side-nav manu control...
-var state = 1;
-button.addEventListener("click", function () {
-    if (state === 1) {
-        document.querySelector(".side-nav").style.display = "flex";
-        state = state - 1;
-    }else if (state === 0) {
-        document.querySelector(".side-nav").style.display = "none";
-        state = state + 1;
+window.addEventListener("scroll", () => {
+    var y = window.scrollY
+    if (y > 80) {
+        nav.classList.add("nav-min")
+        nav.classList.remove("nav-max")
     }
+    else {
+        nav.classList.add("nav-max")
+        nav.classList.remove("nav-min")
+    }
+    console.log(y);
+})
+
+var state = 0
+tab_button.addEventListener("click", () => {
+    if (state === 0) {
+        side_nav.classList.add("mo-tab-out");
+        side_nav.classList.remove("mo-tab-close");
+        state = 1;
+    }else if (state === 1) {
+        side_nav.classList.add("mo-tab-close");
+        side_nav.classList.remove("mo-tab-out");
+        state = 0;
+    } else {
+        log("error");
+    }
+    
 })
