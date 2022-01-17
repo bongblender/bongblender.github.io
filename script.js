2e97138fbc167407ec970158e39569d23f0f3d88
Bongblender <41033357+bongblender@users.noreply.github.com>
2022-01-17 10:58:57 +0530

    update

--- script.js
+++ script.js
@@ -15,32 +15,35 @@ window.addEventListener("scroll", () => {
         nav.classList.add("nav-max")
         nav.classList.remove("nav-min")
     }
-    console.log(lets_talk.scrollY);
+    // console.log(lets_talk.scrollY);
 })
 
 var state = 0
-tab_button.addEventListener("click", () => {
-    if (state === 0) {
-        side_nav.classList.add("mo-tab-out");
-        side_nav.classList.remove("mo-tab-close");
-        tab_button.classList.add("tab-button-rotate")
-        tab_button.classList.remove("tab-button-strait")
-        state = 1;
-    }else if (state === 1) {
-        side_nav.classList.add("mo-tab-close");
-        side_nav.classList.remove("mo-tab-out");
-        tab_button.classList.add("tab-button-strait")
-        tab_button.classList.remove("tab-button-rotate")
-        state = 0;
-    } else {
-        log("error");
-    }
-    
-})
-tab_close.addEventListener("click", () => {
+function tabClose() {
     side_nav.classList.add("mo-tab-close");
     side_nav.classList.remove("mo-tab-out");
     tab_button.classList.add("tab-button-strait")
     tab_button.classList.remove("tab-button-rotate")
     state = 0;
-})
+}
+function tabOpen(params) {
+    side_nav.classList.add("mo-tab-out");
+    side_nav.classList.remove("mo-tab-close");
+    tab_button.classList.add("tab-button-rotate")
+    tab_button.classList.remove("tab-button-strait")
+    state = 1;
+}
+
+// close on clicking on emty space
+document.addEventListener("click", function(e){
+    console.log(e.target.id);
+    if (e.target.id === 'open-close') {
+        if (state === 0){
+            tabOpen()
+        }else if(state === 1){
+            tabClose()
+        }
+    }else{
+        tabClose()
+    }
+});
